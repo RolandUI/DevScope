2f27e9d3303f3222b1e68feafdd90843db52c29c
DearVa <dear.va.love@gmail.com>
2026-06-27 12:22:51 +0800

    fix: `CompiledBinding` is not supported

--- src/Elements/Styles/BindingSetterViewModel.cs
+++ src/Elements/Styles/BindingSetterViewModel.cs
@@ -6,22 +6,40 @@ namespace ClassicDiagnostics.Avalonia.Elements.Styles;
 
 internal class BindingSetterViewModel : SetterViewModel
 {
+    public IBrush? Tint { get; }
+
+    public string? ValueTypeTooltip { get; }
+
+    public string? Path { get; }
+
     public BindingSetterViewModel(AvaloniaProperty property, object? value, IClipboard? clipboard) : base(property, value, clipboard)
     {
         switch (value)
         {
             case Binding binding:
+            {
                 Path = binding.Path;
                 Tint = Brushes.CornflowerBlue;
                 ValueTypeTooltip = "Reflection Binding";
                 break;
-            case CompiledBindingExtension binding:
-                Path = binding.Path!.ToString();
+            }
+            case CompiledBindingExtension compiledBindingExtension:
+            {
+                Path = compiledBindingExtension.Path?.ToString();
                 Tint = Brushes.DarkGreen;
                 ValueTypeTooltip = "Compiled Binding";
                 break;
-            case TemplateBinding binding:
-                if (binding.Property is { } templateProperty)
+            }
+            case CompiledBinding compiledBinding:
+            {
+                Path = compiledBinding.Path?.ToString();
+                Tint = Brushes.DarkGreen;
+                ValueTypeTooltip = "Compiled Binding";
+                break;
+            }
+            case TemplateBinding templateBinding:
+            {
+                if (templateBinding.Property is { } templateProperty)
                 {
                     Path = $"{templateProperty.OwnerType.Name}.{templateProperty.Name}";
                 }
@@ -33,19 +51,31 @@ internal class BindingSetterViewModel : SetterViewModel
                 Tint = Brushes.OrangeRed;
                 ValueTypeTooltip = "Template Binding";
                 break;
+            }
+            case BindingBase bindingBase:
+            {
+                Path = bindingBase.ToString();
+                Tint = Brushes.Gray;
+                ValueTypeTooltip = "Other Binding";
+                break;
+            }
+            case null:
+            {
+                Path = "null";
+                Tint = Brushes.Gray;
+                ValueTypeTooltip = "Null Binding";
+                break;
+            }
             default:
-                throw new ArgumentException("Invalid binding type", nameof(value));
+            {
+                DevToolsDiagnostics.Report(new ArgumentException("Invalid binding type", nameof(value)), $"Invalid binding type: {value}");
+                break;
+            }
         }
     }
 
-    public IBrush Tint { get; }
-
-    public string ValueTypeTooltip { get; }
-
-    public string Path { get; }
-
     public override void CopyValue()
     {
-        CopyToClipboard(Path);
+        if (!string.IsNullOrEmpty(Path)) CopyToClipboard(Path);
     }
 }
